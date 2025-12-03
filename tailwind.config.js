9a7c319cb2f012ed4475fc1f9d21dbe8759ba12c
Adk-gh <dekbermas@gmail.com>
2025-12-03 18:38:29 +0800

    gagaa

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./frontend/src/**/*.{js,ts,jsx,tsx}",  // Added just in case your structure is nested
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
